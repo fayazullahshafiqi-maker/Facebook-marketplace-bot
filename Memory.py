b4582c30832873f3baa8c47b2fde9dd8ddd8240b
fayazullahshafiqi-maker <fayazullahshafiqi@gmail.com>
2026-05-03 09:50:03 +1000

    Create Memory.py

---
+++ Memory.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+MEMORY_FILE = "deal_memory.json"
+
+def load_memory():
+    if os.path.exists(MEMORY_FILE):
+        with open(MEMORY_FILE, "r") as f:
+            return json.load(f)
+
+    return {
+        "offers": {},
+        "deals": []
+    }
+
+def save_memory(memory):
+    with open(MEMORY_FILE, "w") as f:
+        json.dump(memory, f, indent=2)
+
+memory = load_memory()
+
+def store_result(model, offer, result):
+    memory["offers"].setdefault(model, [])
+
+    memory["offers"][model].append({
+        "offer": offer,
+        "result": result
+    })
+
+    save_memory(memory)
