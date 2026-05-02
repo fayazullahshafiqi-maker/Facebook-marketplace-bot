cab092d1730748df2e8184876787ca86e7b5e685
fayazullahshafiqi-maker <fayazullahshafiqi@gmail.com>
2026-05-03 01:48:02 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+BOT_TOKEN = "7862438221:AAGOBsSyiwey2IANiHHTi_tlDBPDUi-l9Zc"
+CHAT_ID = "7403593902"
+
+def send_test():
+
+    msg = "🤖 Bot is running successfully (NSW system connected)"
+
+    requests.post(
+
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+
+        data={"chat_id": CHAT_ID, "text": msg}
+
+    )
+
+send_test()
